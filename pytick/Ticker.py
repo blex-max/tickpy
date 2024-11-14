a3cb8284c1f0416896c0366600e155c2b14ede25
blex-max <ablex7@gmail.com>
2024-11-14 15:51:34 +0000

    extend counter comp to give options for blocking, nonblocking checks

--- pytick/Ticker.py
+++ pytick/Ticker.py
@@ -6,27 +6,45 @@ class Ticker:
         self.tick_interval = tick_interval_s
         self.counter = 0
         self.last_tick_time = None
+        self.started = False
         self.__block_flags = {}
 
     def start(self):
         self.last_tick_time = time.time()
+        self.started = True
 
     def update(self) -> tuple[bool, int]:
+        if not self.started:
+            raise RuntimeError('ticker not started, call .start() before use')
         now = time.time()
         elapsed_t = now - self.last_tick_time
         if elapsed_t >= self.tick_interval:
             self.counter += 1
             self.last_tick_time = now
+        for k in self.__block_flags:
+            if self.counter % k != 0 and self.__block_flags[k]:
+                self.__block_flags[k] = False
 
-    def counter_comp(self, mod):
+    def counter_comp(self,
+                     mod,
+                     chk: bool = True,
+                     blk: bool = True,
+                     unblk: bool = True):
         try:
             self.__block_flags[mod]
         except KeyError:
             self.__block_flags[mod] = None
         if self.counter % mod == 0:
-            if not self.__block_flags[mod]:
-                self.__block_flags[mod] = True
+            # falls through to final return False
+            if chk and not self.__block_flags[mod]:
+                if blk:
+                    self.__block_flags[mod] = True
                 return True
-        if self.counter % mod != 0 and self.__block_flags[mod]:
+            elif not chk:
+                if blk and not self.__block_flags[mod]:
+                    self.__block_flags[mod] = True
+                return True
+        # could tie yourself in knots here, fragile
+        if self.counter % mod != 0 and self.__block_flags[mod] and unblk:
             self.__block_flags[mod] = False
         return False
