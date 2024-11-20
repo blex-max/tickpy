f91b479fde65cfb0e8085adfe236a5d2ec8237a7
blex-max <ablex7@gmail.com>
2024-11-20 10:20:32 +0000

    some testing, a few different classes depending on need

--- pytick/ticker.py
+++ pytick/ticker.py
@@ -1,35 +1,48 @@
-import time
+from time import perf_counter
+
+
+class MinTicker:
+    def __init__(self,
+                 tick_interval_s: float):
+        self.tick_interval = tick_interval_s
+        self.counter: int = 0
+        self.start_time = perf_counter()
+
+    def update(self):
+        self.counter = int((perf_counter() - self.start_time) / self.tick_interval)
+
+    def mod(self,
+            mod: int):
+        if self.counter % mod == 0:
+            return True
+        return False
 
 
 class Ticker:
-    def __init__(self, tick_interval_s):
+    def __init__(self,
+                 tick_interval_s: float):
         self.tick_interval = tick_interval_s
-        self.counter = 0
-        self.last_tick_time = None
-        self.started = False
+        self.counter: int = 0
+        self.start_time = perf_counter()
         self.__block_flags = {}
 
-    def start(self):
-        self.last_tick_time = time.time()
-        self.started = True
-
-    def update(self) -> tuple[bool, int]:
-        if not self.started:
-            raise RuntimeError('ticker not started, call .start() before use')
-        now = time.time()
-        elapsed_t = now - self.last_tick_time
-        if elapsed_t >= self.tick_interval:
-            self.counter += 1
-            self.last_tick_time = now
+    def update(self):
+        self.counter = int((perf_counter() - self.start_time) / self.tick_interval)
         for k in self.__block_flags:
             if self.counter % k != 0 and self.__block_flags[k]:
                 self.__block_flags[k] = False
 
-    def counter_comp(self,
-                     mod,
-                     chk: bool = True,
-                     blk: bool = True,
-                     unblk: bool = True):
+    def mod(self,
+            mod: int):
+        if self.counter % mod == 0:
+            return True
+        return False
+
+    def cmod(self,
+             mod: int,
+             chk: bool = True,
+             blk: bool = True,
+             unblk: bool = True):
         try:
             self.__block_flags[mod]
         except KeyError:
@@ -45,6 +58,57 @@ class Ticker:
                     self.__block_flags[mod] = True
                 return True
         # could tie yourself in knots here, fragile
-        if self.counter % mod != 0 and self.__block_flags[mod] and unblk:
+        if unblk and self.__block_flags[mod] and self.counter % mod != 0:
+            self.__block_flags[mod] = False
+        return False
+
+
+class IncTicker:
+    def __init__(self,
+                 tick_interval_s: float):
+        self.tick_interval = tick_interval_s
+        self.counter: int = 0
+        self.last_tick_time: float = perf_counter()
+        self.__block_flags: dict[int, bool | None] = {}
+
+
+    # should this catch up, or only increment once?
+    def update(self):
+        now = perf_counter()
+        elapsed_t = now - self.last_tick_time  # type: ignore
+        if elapsed_t >= self.tick_interval:
+            self.counter += 1
+            self.last_tick_time = now
+        for k in self.__block_flags:
+            if self.counter % k != 0 and self.__block_flags[k]:
+                self.__block_flags[k] = False
+
+    def mod(self,
+            mod: int):
+        if self.counter % mod == 0:
+            return True
+        return False
+
+    def cmod(self,
+             mod: int,
+             chk: bool = True,
+             blk: bool = True,
+             unblk: bool = True):
+        try:
+            self.__block_flags[mod]
+        except KeyError:
+            self.__block_flags[mod] = None
+        if self.counter % mod == 0:
+            # falls through to final return False
+            if chk and not self.__block_flags[mod]:
+                if blk:
+                    self.__block_flags[mod] = True
+                return True
+            elif not chk:
+                if blk and not self.__block_flags[mod]:
+                    self.__block_flags[mod] = True
+                return True
+        # could tie yourself in knots here, fragile
+        if unblk and self.__block_flags[mod] and self.counter % mod != 0:
             self.__block_flags[mod] = False
         return False
---
+++ tests/test_IncTicker.py
@@ -0,0 +1,4 @@
+from pytick.ticker import IncTicker
+import time
+
+
