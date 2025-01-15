e49d0620f2cbeaa0bf0aee34c08e0efefa606cff
blex-max <ablex7@gmail.com>
2025-01-15 13:22:53 +0000

    formalise structure

--- tickpy/ticker.py
+++ tickpy/ticker.py
@@ -1,7 +1,9 @@
 from time import perf_counter
 
-
-class MinTicker:
+class Ticker:
+    """
+    Basic "ticker" timer, i.e. will increment a counter tracking a given period.
+    """
     def __init__(self,
                  tick_interval_s: float):
         self.tick_interval = tick_interval_s
@@ -12,7 +14,6 @@ class MinTicker:
         prev = self.counter
         self.counter = int((perf_counter() - self.start_time) / self.tick_interval)
         return True if self.counter != prev else False
-
 
     def mod(self,
             mod: int):
@@ -20,40 +21,32 @@ class MinTicker:
             return True
         return False
 
-
-class Ticker:
-    def __init__(self,
-                 tick_interval_s: float):
-        self.tick_interval = tick_interval_s
-        self.counter: int = 0
-        self.start_time = perf_counter()
-        self.__block_flags = {}
-
-    def update(self):
-        prev = self.counter
-        self.counter = int((perf_counter() - self.start_time) / self.tick_interval)
-        for k in self.__block_flags:
-            if self.counter % k != 0 and self.__block_flags[k]:
-                self.__block_flags[k] = False
-        return True if self.counter != prev else False
-
-    def since(self, period_start: int) -> int:
+    def since(self, period_start: int | None = None) -> int:
+        period_start = period_start if period_start else 0  # 0 == start
         return self.counter - period_start
 
-    def elapsed(self, period_len: int, period_start: int) -> bool:
+    def elapsed(self, period_len: int, period_start: int | None = None) -> bool:
+        """
+        Has a given period elapsed since period_start?
+        """
         return True if self.since(period_start) >= period_len else False
 
-    def mod(self,
-            mod: int):
-        if self.counter % mod == 0:
-            return True
-        return False
+
+class __TickerIntermediary(Ticker):
+    """
+    Private class to share functionality between child classes
+    """
+    def __init__(self,
+                 tick_interval_s: float):
+        super().__init__(tick_interval_s)
+        self.__block_flags = {}
 
     def cmod(self,
              mod: int,
              chk: bool = True,
              blk: bool = True,
              unblk: bool = True):
+        # may implement a "cmod since" functionality
         try:
             self.__block_flags[mod]
         except KeyError:
@@ -74,16 +67,31 @@ class Ticker:
         return False
 
 
-class IncTicker:
+class ExtTicker(__TickerIntermediary):
+    """
+    Ticker with extended functionality - internally handled checks for a period having elapsed which avoid returning True more than once in a while loop.
+    """
     def __init__(self,
                  tick_interval_s: float):
-        self.tick_interval = tick_interval_s
-        self.counter: int = 0
-        self.last_tick_time: float = perf_counter()
-        self.__block_flags: dict[int, bool | None] = {}
+        super().__init__(tick_interval_s)
+
+    def update(self):
+        ticked = super().update()
+        for k in self.__block_flags:
+            if self.counter % k != 0 and self.__block_flags[k]:
+                self.__block_flags[k] = False
+        return ticked
 
 
-    # should this catch up, or only increment once?
+class IncTicker(__TickerIntermediary):
+    """
+    Ticker with identical functionality to ExtTicker, excepting that when, upon calling update(), the tick interval has elapsed since the last call to update(), the ticker will *only* increment .counter by one, regardless of how much time has actually elapsed.
+    """
+    def __init__(self,
+                 tick_interval_s: float):
+        super().__init__(tick_interval_s)
+        self.last_tick_time: float = self.start_time
+
     def update(self):
         prev = self.counter
         now = perf_counter()
@@ -96,32 +104,3 @@ class IncTicker:
                 self.__block_flags[k] = False
         return True if self.counter != prev else False
 
-    def mod(self,
-            mod: int):
-        if self.counter % mod == 0:
-            return True
-        return False
-
-    def cmod(self,
-             mod: int,
-             chk: bool = True,
-             blk: bool = True,
-             unblk: bool = True):
-        try:
-            self.__block_flags[mod]
-        except KeyError:
-            self.__block_flags[mod] = None
-        if self.counter % mod == 0:
-            # falls through to final return False
-            if chk and not self.__block_flags[mod]:
-                if blk:
-                    self.__block_flags[mod] = True
-                return True
-            elif not chk:
-                if blk and not self.__block_flags[mod]:
-                    self.__block_flags[mod] = True
-                return True
-        # could tie yourself in knots here, fragile
-        if unblk and self.__block_flags[mod] and self.counter % mod != 0:
-            self.__block_flags[mod] = False
-        return False
--- tickpy/timer.py
+++ tickpy/timer.py
@@ -1,6 +1,9 @@
 from time import perf_counter
 
 class Timer:
+    """
+    Normal Timer, i.e. .now is always now
+    """
     def __init__(self):
         self.start_time: float = perf_counter()
 
@@ -15,17 +18,14 @@ class Timer:
         return True if self.since((period_start if period_start else self.start_time))>= period_len else False
 
 
-class StaticTimer:
+class StaticTimer(Timer):
+    """
+    Timer that only updates its reference point when instructed, i.e. .now refers to the last time the timer was instructed to update
+    """
     def __init__(self):
-        self.start_time: float = perf_counter()
-        self.now = self.start_time
-
-    def since(self, past_t: float | None = None) -> float:
-        return self.now - (past_t if past_t else self.start_time)
-
-    def elapsed(self, period_len: float, period_start: float | None = None) -> bool:
-        return True if self.since((period_start if period_start else self.start_time))>= period_len else False
+        super().__init__()
+        self.now = self.start_time  # type: ignore
 
     def update(self):
-        self.now = perf_counter()
+        self.now = perf_counter()  # type: ignore
     
